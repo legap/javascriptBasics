4d4a6b2ae8ed63fa3bac0c8cbcb3e2e293ce51b6
Pascal Gafner <gafner.pascal@gmail.com>
2016-02-24 22:49:51 +0100

    corrected deathValleyForLoop.js to multiply before logging to the console

--- deathValleyForLoop.js
+++ deathValleyForLoop.js
@@ -2,6 +2,6 @@ var numSheep = 4;
 var monthsToPrint = 12;
 
 for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
-    console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
     numSheep *= 4;
+    console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
 }
